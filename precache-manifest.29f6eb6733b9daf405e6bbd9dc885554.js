380f34db37c87e0ec2e23f2d1ce3bb79aeea92e7
Eric <eric1162004@hotmail.com>
2020-04-09 20:51:52 -0700

    Updates

---
+++ precache-manifest.29f6eb6733b9daf405e6bbd9dc885554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78a22281159204336ce63a13fab9ca2",
+    "url": "/booklist-app/index.html"
+  },
+  {
+    "revision": "ebdf21c12a6ebe81f510",
+    "url": "/booklist-app/static/css/main.f7e1efb5.chunk.css"
+  },
+  {
+    "revision": "da75fcf3d6e5fb0d903d",
+    "url": "/booklist-app/static/js/2.96e20730.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/booklist-app/static/js/2.96e20730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdf21c12a6ebe81f510",
+    "url": "/booklist-app/static/js/main.fe3b2558.chunk.js"
+  },
+  {
+    "revision": "99734e139182b7585ac7",
+    "url": "/booklist-app/static/js/runtime-main.fd934b1a.js"
+  }
+]);
